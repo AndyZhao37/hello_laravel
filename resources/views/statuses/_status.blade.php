d42840588ffb47364526e07a67c2ba57f5a9be25
Andy zhao <1976909798@qq.com>
2020-04-03 07:07:00 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -3,4 +3,11 @@
     <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
     {{ $status->content }}
   </div>
+  @can('destroy', $status)
+<form method="POST" action="{{ route('statuses.destroy', $status->id) }}">
+  {{ csrf_field() }}
+  {{ method_field('DELETE') }}
+  <button type="submit" class="btn btn-sm btn-danger">删除</button>
+</form>
+  @endcan
 </li>
